10a43d0a07fadfa5ff379a523967a3df5692f41d
Taposh <taposhsorkar88@gmail.com>
2024-01-22 20:52:54 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+(function($){
+	$(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrolltotop').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrolltotop').fadeIn();
+			} else {
+				jQuery('.scrolltotop').fadeOut();
+			}
+		});
+
+		
+		// count up
+		$('.timer').appear();
+		    $(document.body).on('appear', '.timer', function(e, $affected) {
+		        $affected.each(function() {
+		            var $this = $(this);
+		            if (!$this.hasClass('appeared')) {
+		                jQuery({
+		                    Counter: 0
+		                }).animate({
+		                    Counter: $this.attr('data-count')
+		                }, {
+		                    duration: 1200,
+		                    easing: 'swing',
+		                    step: function() {
+		                        var num = Math.ceil(this.Counter).toString();
+		                        $this.html(num);
+		                    }
+		                });
+		                $this.addClass('appeared');
+		            }
+		        });
+		    });	
+
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery);
+
+	// page loader
+	$(window).on('load',function(){
+		setTimeout(function(){ 
+		$('.page-loader').fadeOut('slow');
+		},500);
+	});
+
+	// disbale code inspect
+
+
+	// take body to change the content
+	// const body = document.getElementsByTagName('body');
+
+	// stop keyboard shortcuts
+
+	// window.addEventListener("keydown", (event) => {
+	// if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
+	// 	event.preventDefault();
+	// }
+	// if(event.ctrlKey && (event.key === "C")) {
+	// 	event.preventDefault();
+	// }
+	// if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
+	// 	event.preventDefault();
+	// }
+	// if(event.ctrlKey && (event.key === "I" || event.key === "i")) {
+	// 	event.preventDefault();
+	// }
+	// if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
+	// 	event.preventDefault();
+	// }
+	// if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
+	// 	event.preventDefault();
+	// }
+	// if (event.key === "F12") {
+    //     event.preventDefault();
+    // }
+	// });
+	// stop right click
+	// document.addEventListener('contextmenu', function(e) {
+	// e.preventDefault();
+	// });
+
+
